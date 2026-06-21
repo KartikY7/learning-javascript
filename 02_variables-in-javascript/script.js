1ec15216ffbaf758b87381d49abac94cf2dfb657
Kartik <kartiky14012002@gmail.com>
2026-06-21 19:54:22 +0530

    practice javascript variables

---
+++ 02_variables-in-javascript/script.js
@@ -0,0 +1,19 @@
+// console.log(hoursInDay) 
+
+
+var firstName = 'Kartik'
+let lastName = 'Yadav'
+
+let age = 21
+let isHappy = true
+firstName = 100
+var a
+// a = 'Kashish'
+
+const hoursInDay = 24
+
+// hoursInDay = 18
+
+let userIntro = 'Hi, my name is ' 
++ firstName  + ' ' 
++ lastName + '. I am ' + age + ' years old.'
