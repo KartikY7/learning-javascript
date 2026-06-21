37b0e4b940e63ce4e0ad5c9ac7fc905dd32b3750
Kartik <kartiky14012002@gmail.com>
2026-06-21 20:11:41 +0530

    practice ternary operator

---
+++ 14_ternary-operator/script.js
@@ -0,0 +1,13 @@
+// debugger
+// const username = 5 > 2 ? 'Kartik' : 'ProCoder'
+
+// const gender = 'F'
+
+// debugger
+// const userMessage = `${gender.toLocaleLowerCase() === 'f' ? 'She' : 'He'} is a college student.`
+
+// console.log(userMessage);
+
+const result = null ? 'Kartik' : '' ? '12' : 0
+
+console.log(result);
