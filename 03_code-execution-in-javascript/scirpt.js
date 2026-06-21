51f34fa1e197f9e658021265f930a22052d44541
Kartik <kartiky14012002@gmail.com>
2026-06-21 19:54:58 +0530

    understand code execution in devtools

---
+++ 03_code-execution-in-javascript/scirpt.js
@@ -0,0 +1,9 @@
+debugger
+console.log( firstName)
+
+var firstName = 'Ankur'
+let lastName = 'Singh'
+let age = 18
+const yearofBirth = 2001
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
