14794ead6deca1d9672c5ead7db84ab45277cf90
Kartik <kartiky14012002@gmail.com>
2026-06-25 18:48:28 +0530

    understand multidimensional array

---
+++ 20_multidimensional-arrays/script.js
@@ -0,0 +1,11 @@
+const nameAndHeight = [['Aman', 172], ['Raghav', 170]]
+
+// const firstRow = [['X'], [], []]
+// const secondRow = [[], [], ['O']]
+// const thirdRow = [['O'], [], ['X']]
+
+const ticTacToe = [
+    ['X', null, null],
+    [null, null, 'O'],
+    ['O', null, 'X'] 
+]
