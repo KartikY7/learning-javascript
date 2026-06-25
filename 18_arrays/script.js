94a8f276c2100221845a182c7b5f3a575bbaf277
Kartik <kartiky14012002@gmail.com>
2026-06-25 11:09:49 +0530

    understand arrays

---
+++ 18_arrays/script.js
@@ -0,0 +1,8 @@
+const username = 'Kartik Yadav'
+
+const fruitsCollection = ['Apple', 'Banana', 'Grapes', 'Orange']
+
+const rainbowcolors = ['V', 'I', 'B', 'G', 'Y', 'O', 'R']
+
+const newObject = {}
+const newArray = []
