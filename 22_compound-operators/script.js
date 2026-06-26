19d5cd9d002604e9bb4f811aa4c998352d6146b1
Kartik <kartiky14012002@gmail.com>
2026-06-26 18:51:13 +0530

    practice compound operators and ++/--

---
+++ 22_compound-operators/script.js
@@ -0,0 +1,55 @@
+const userName = 'Kartik'
+
+let num = 5
+
+// debugger
+// num = num + 2
+// num = num + 2
+
+// num += 5
+// num += 5
+
+// const newNum = num++
+// const newNum = ++num
+
+// const newNum = --num
+// const newNum = num--
+
+// num = num + 1
+// num += 1
+
+// num++
+
+
+// num = num - 2
+// num = num - 2
+
+
+// num -= 2
+// num -= 2
+
+
+// num = num % 2 
+// num %= 2
+
+
+// num = num * 2
+// num = num * 2
+
+
+// num *= 2
+// num *= 2
+
+
+// num = num / 2
+// num = num / 2
+
+// num /= 2
+// num /= 2
+
+
+// num = num ** 2
+// num = num ** 2
+
+// num **= 2
+// num **= 2
