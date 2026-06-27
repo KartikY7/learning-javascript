d52904493e6b56304eba2a73fcfa53318564e7eb
Kartik <kartiky14012002@gmail.com>
2026-06-27 18:37:01 +0530

    understand for loop

---
+++ 24_for-loop/script.js
@@ -0,0 +1,27 @@
+console.log('Program Start');
+
+// debugger
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i + 1);
+// }
+
+
+const friends = ['Aman', 'Raj', 'Rohan', 'Prem', 'Rahul', 'Kartik', 'Raghav']
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(`${i + 1}. ${friends[i]} Yadav`);
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}    
+
+
+// for (let i = 0; i <= 100; i += 2)
+//     console.log(i);
+
+console.log('Program Ended');
