ca83693415893d649707ba68371fea36d29ceaf0
Kartik <kartiky14012002@gmail.com>
2026-06-29 14:10:38 +0530

    learn javascript scope

---
+++ 27_functions-with-return-value/script.js
@@ -0,0 +1,20 @@
+function addTwoNumbers(a, b) {
+//    debugger
+    return a + b
+}
+
+const result = addTwoNumbers(45, 5)
+
+console.log(result);
+
+
+
+
+
+// function subtractTwoNumbers(a, b) {
+//     return a - b
+// } 
+
+// // const result = subtractTwoNumbers(45, 5)
+
+// // console.log(result);
