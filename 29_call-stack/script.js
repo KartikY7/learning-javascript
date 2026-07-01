5df47db742a25d25cad57ba5c7fccc0fdd7e535f
Kartik <kartiky14012002@gmail.com>
2026-07-01 09:19:45 +0530

    understand call stack

---
+++ 29_call-stack/script.js
@@ -0,0 +1,25 @@
+debugger
+
+// sayHi()
+
+// const username = 'Anurag'
+// const userAge = 25
+
+// function sayHi() {
+//     const a = 14
+//     const b = 12
+//     add(7, 9)
+// }
+
+// function add(x, y) {
+//     return x + y
+// }
+
+function introduceMe() {
+    console.log('Hi, My name is Kartik.');
+    introduceMe()
+}
+
+introduceMe()
+
+console.log('Program Ended');
