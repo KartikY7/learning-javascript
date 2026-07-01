936944d88c157c3a9d99f5f07b57c592c2a31a22
Kartik <kartiky14012002@gmail.com>
2026-07-01 08:30:39 +0530

    understand execution context

---
+++ 28_execution-context-in-javascript/script.js
@@ -0,0 +1,18 @@
+debugger
+
+sayHi('Hi')
+
+const userName = 'Kartik'
+const userAge = 21
+
+function sayHi() {
+    const a = 4
+    const b = 5
+    addTwoNumber(7, 9)
+}
+
+function addTwoNumber(x, y) {
+    return x + y 
+}
+
+console.log('Program Ended');
