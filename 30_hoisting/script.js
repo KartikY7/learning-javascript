27db5e26962f13d0aa2967a0dcfa990e9ef12b6f
Kartik <kartiky14012002@gmail.com>
2026-07-02 23:47:59 +0530

    understand hoisting

---
+++ 30_hoisting/script.js
@@ -0,0 +1,23 @@
+// debugger
+
+console.log(a)
+
+var a = 'Kartik'
+
+hi()
+
+
+// Function Definition
+// Function Declaration
+function hi() {
+    console.log('Hello');
+}
+
+// Function Definition
+// Function Expression
+var sayHi = function() {    //anonymous function
+    console.log('Hiii');
+}
+
+
+sayHi()
