503d7cddd2d23f26e7eea3164545dbdf8edf4eab
Kartik <kartiky14012002@gmail.com>
2026-07-04 09:56:02 +0530

    understand global and local scope

---
+++ 31_scope-in-javascript/script.js
@@ -0,0 +1,27 @@
+// debugger
+
+const username = 'Kartik'
+let userAge = 21
+var a = 55
+
+function add() {
+    const username = 'Aman'
+    const x = 5
+    const y = 8
+    console.log(x + y)
+    console.log(username);
+}
+
+function subtract() {
+    const x = 5
+    const y = 8
+    console.log(x - y)
+    console.log(username);
+}
+
+
+add()
+
+subtract()
+
+console.log('Program Ended');
