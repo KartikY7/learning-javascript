4d776bd11e14789c6ef987a2ad9726f805d9b0df
Kartik <kartiky14012002@gmail.com>
2026-07-05 15:55:48 +0530

    understand lexical and block scope

---
+++ 32_lexical-and-block-scope/script.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const username = 'Anurag'
+let userAge = 25
+var a = 50
+
+// function add() {
+//   const username = 'Akash'
+//   const x = 5
+//   const y = 8
+//   console.log(x + y)
+//   console.log(username)
+// }
+
+function subtract() {
+    
+    const x = 15
+    const y = 18
+    const z = 10
+    
+    console.log(x - y)
+    console.log(username)
+    
+    function child() {
+    // debugger
+    const childName = 'Ram'
+    console.log(childName);
+    console.log(z);
+
+    if(true){
+        let num1 = 75
+        var num2 = 324
+        console.log(num1);
+        console.log(num2);
+    }
+
+        console.log(num2);
+
+
+    function grandChild() {
+        const grandChildName = 'Raj'
+        console.log(grandChildName);
+    }
+    grandChild( )
+  }
+
+    child()
+
+}
+
+
+// add()
+subtract()
+
+console.log('Program Ended')
